bfcca3b1bc4cebb0a89bbd30227a35280a179ce1
addankiashok2 <ashokaddanki602@gmail.com>
2022-08-21 14:18:21 +0530

    added new rest api to the branch

---
+++ todo_api/app.py
@@ -0,0 +1,80 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse, abort, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite.db'
+db = SQLAlchemy(app)
+
+class ToDoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(200))
+    summary = db.Column(db.String(500))
+# db.create_all()
+
+resource_fields = {
+    'id':fields.Integer,
+    'task':fields.String,
+    'summary':fields.String
+}
+
+task_post_args = reqparse.RequestParser()
+task_post_args.add_argument('task', type=str, help='Task is Required', required=True)
+task_post_args.add_argument('summary', type=str, help='summary is Required', required=True)
+
+task_put_args = reqparse.RequestParser()
+task_put_args.add_argument('task', type=str)
+task_put_args.add_argument('summary', type=str)
+
+class ToDoList(Resource):
+    def get(self):
+        tasks = ToDoModel.query.all()
+        todos = {}
+        for task in tasks:
+            todos[task.id]={'task':task.task, 'summary':task.summary}   
+        return todos
+
+class ToDo(Resource):
+    @marshal_with(resource_fields)
+    def get(self, todo_id):
+        task = ToDoModel.query.filter_by(id=todo_id).first()
+        if not task:
+            abort(404, message='colud not find the task')
+        return task
+    
+    @marshal_with(resource_fields)
+    def post(self, todo_id):
+        args = task_post_args.parse_args()
+        task = ToDoModel.query.filter_by(id=todo_id).first()
+        if task:
+            abort(409, message='task taken alredy')
+        todo = ToDoModel(id=todo_id, task=args['task'], summary=args['summary'])
+        db.session.add(todo)
+        db.session.commit()
+        return todo, 201
+
+    @marshal_with(resource_fields)
+    def put(self, todo_id):
+        args = task_put_args.parse_args()
+        task = ToDoModel.query.filter_by(id=todo_id).first()
+        if not task:
+            abort(404, message='could not modify task alredy exist')
+        if args['task']:
+            task.task = args['task']
+        if args['summary']:
+            task.summary = args['summary']
+        db.session.commit()
+        return task
+
+    def delete(self, todo_id):
+        task = ToDoModel.query.filter_by(id=todo_id).first()
+        db.session.delete(task)
+        return "Todo Deleted", 204
+
+api.add_resource(ToDo, '/todos/<int:todo_id>')
+api.add_resource(ToDoList, '/todos')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
